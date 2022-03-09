047f8a9b06156c45c8dfb4f39fc7d4bc5fdf60be
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-03-09 09:02:37 +0200

    refactor: :construction: Added Favicon, Doc title and basic component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MangaList from "./Components/MangaList";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <MangaList />
+    </div>
+  );
 }
 
 export default App;
