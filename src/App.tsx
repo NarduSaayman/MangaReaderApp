1d62d8b1af7cb8058ff5d47980d4221a1431a009
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-03-09 14:08:54 +0200

    feat: :construction: Added Header elements and Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import MangaCard from "./Components/MangaCard";
+import logo from "./assets/images/logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <MangaCard />
+      <div id="Header">
+        <div id="Logo">
+          <div className="">
+            <img className="" src={logo} alt="" />
+          </div>
+          <h1>TikesReader</h1>
+        </div>
+        <div>
+          <input type="text" />
+        </div>
+        <div id="Nav" />
+      </div>
     </div>
   );
 }
